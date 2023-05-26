10752f7cdfd1e58307c56b61e2715bdcbe12f3e7
“Your <ducanh2kqo@gmail.com>
2023-05-26 10:40:40 +0700

    fix: fix something in RadioGroup component

--- src/components/RadioGroup/index.tsx
+++ src/components/RadioGroup/index.tsx
@@ -1,28 +1,50 @@
 import { Radio, RadioChangeEvent, Space } from 'antd'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
-const RadioGroup = () => {
-  const [value, setValue] = useState(0)
-  const onChange = (e: RadioChangeEvent) => {
-    console.log('radio checked', e.target.value)
-    setValue(e.target.value)
+type Props = {
+  value?: string | number
+  options?: { label: React.ReactNode; value: string | number }[]
+  onChange?: (value: string | number) => void
+  disabled?: boolean
+  optionClassName?: string
+}
+
+const RadioGroup = ({
+  value,
+  options,
+  onChange,
+  disabled,
+  optionClassName,
+}: Props) => {
+  const [radioValue, setRadioValue] = useState(value)
+
+  const handleChange = (e: RadioChangeEvent) => {
+    setRadioValue(e.target.value)
+    onChange?.(e.target.value)
   }
+
+  const memoValue = useMemo(() => {
+    if (value === undefined) return radioValue
+    return value
+  }, [radioValue, value])
+
   return (
     <div>
-      <Radio.Group onChange={onChange} value={value}>
+      <Radio.Group
+        onChange={handleChange}
+        value={memoValue}
+        disabled={disabled}
+      >
         <Space direction="vertical">
-          <Radio className="flex gap-[20px]" value={1}>
-            Đáp án A
-          </Radio>
-          <Radio className="flex gap-[20px]" value={2}>
-            Đáp án B
-          </Radio>
-          <Radio className="flex gap-[20px]" value={3}>
-            Đáp án C
-          </Radio>
-          <Radio className="flex gap-[20px]" value={4}>
-            Đáp án D
-          </Radio>
+          {options?.map(option => (
+            <Radio
+              key={option.value}
+              className="flex gap-[20px]"
+              value={option.value}
+            >
+              {option.label}
+            </Radio>
+          ))}
         </Space>
       </Radio.Group>
     </div>
