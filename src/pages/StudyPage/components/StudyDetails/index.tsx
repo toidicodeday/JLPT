81569a68fc03c9acdd0d8064e7948b64e2126776
“Your <ducanh2kqo@gmail.com>
2023-05-30 15:25:34 +0700

    fix: split exercise data and css

--- src/pages/StudyPage/components/StudyDetails/index.tsx
+++ src/pages/StudyPage/components/StudyDetails/index.tsx
@@ -5,57 +5,85 @@ import { useNavigate } from 'react-router-dom'
 
 const StudyDetails = () => {
   const navigate = useNavigate()
-  const exerciseList = [
+  const tagStyle = [
     {
       id: 1,
       tagColor: '#FF261F',
-      textColor: 'white',
+      textClassName: '#fff',
+      marginLeft: '0',
+    },
+    {
+      id: 2,
+      tagColor: '#FF261F',
+      textClassName: '#fff',
+      marginLeft: '48px',
+    },
+    {
+      id: 3,
+      tagColor: '#FFE7EB',
+      textClassName: '#000',
+      marginLeft: '96px',
+    },
+    {
+      id: 4,
+      tagColor: '#FFE7EB',
+      textClassName: '#000',
+      marginLeft: '140px',
+    },
+    {
+      id: 5,
+      tagColor: '#8EF9F3',
+      textClassName: '#000',
+      marginLeft: '96px',
+    },
+    {
+      id: 6,
+      tagColor: '#593C8F',
+      textClassName: '#fff',
+      marginLeft: '48px',
+    },
+    {
+      id: 7,
+      tagColor: '#171738',
+      textClassName: '#fff',
+      marginLeft: '0',
+    },
+  ]
+  const exerciseList = [
+    {
+      id: 1,
       name: ' Bài tập Kanji - Cách đọc Kanji',
       translateX: '0%',
     },
     {
       id: 2,
-      tagColor: '#FF261F',
-      textColor: 'white',
       name: ' Bài tập Kanji - Chọn Kanji phù hợp',
-      translateX: '10%',
     },
     {
       id: 3,
-      tagColor: '#FFE7EB',
-      textColor: 'black',
       name: ' Bài tập Từ vựng - Chọn từ đúng nghĩa',
-      translateX: '20%',
     },
     {
       id: 4,
-      tagColor: '#FFE7EB',
-      textColor: 'black',
       name: ' Bài tập Từ vựng - Chọn từ đồng nghĩa',
-      translateX: '30%',
     },
     {
       id: 5,
-      tagColor: '#8EF9F3',
-      textColor: 'white',
       name: ' Bài tập Ngữ pháp',
-      translateX: '20%',
     },
     {
       id: 6,
-      tagColor: '#593C8F',
-      textColor: 'white',
       name: ' Bài tập Đọc hiểu',
-      translateX: '10%',
     },
     {
       id: 7,
-      tagColor: '#171738',
-      textColor: 'white',
       name: 'Bài tập Nghe hiểu',
-      translateX: '0%',
     },
   ]
+
+  const goToExecisesDetail = () => {
+    navigate('/exercise/exercise-details')
+  }
 
   return (
     <div className="w-full">
@@ -68,7 +96,7 @@ const StudyDetails = () => {
         gutter={[{ xl: 50, lg: 30 }, 30]}
         className="lg:py-32 max-lg:py-20 max-sm:py-10 lg:px-20 max-lg:px-20 max-md:px-10 max-sm:px-5"
       >
-        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+        <Col xs={24} lg={12}>
           <div className="flex items-center justify-center w-full h-full max-xl:py-24 max-lg:py-0">
             <div className="w-[85%] h-[85%] rounded-[50%] border-dashed border-[#FF261F96] flex items-center justify-center">
               <div className="w-[90%] h-[90%] rounded-[50%] border-dashed border-[#8EF9F3] flex items-center justify-center">
@@ -81,26 +109,27 @@ const StudyDetails = () => {
             </div>
           </div>
         </Col>
-        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
-          <div className="">
-            {exerciseList.map(item => (
-              <div
-                key={item.id}
-                className={`mb-7 max-lg:translate-x-0 translate-x-[${item.translateX}]`}
+        <Col xs={24} lg={12}>
+          {exerciseList.map((item, index) => (
+            <div
+              key={item.id}
+              className="mb-7 max-lg:ml-0"
+              style={{
+                marginLeft: tagStyle[index].marginLeft,
+              }}
+            >
+              <Tag
+                onClick={goToExecisesDetail}
+                className={`max-lg:w-full py-4 px-5 rounded-xl font-semibold xl:text-xl max-xl:text-lg max-[500px]:text-xs cursor-pointer`}
                 style={{
-                  transform: `translateX(${item.translateX})`,
+                  backgroundColor: tagStyle[index].tagColor,
+                  color: tagStyle[index].textClassName,
                 }}
               >
-                <Tag
-                  onClick={() => navigate('/exercise/exercise-details')}
-                  className={`max-lg:w-full py-4 px-5 rounded-[10px] text-${item.textColor} font-semibold text-xl sm:text-lg max-sm:text-xs cursor-pointer`}
-                  color={item.tagColor}
-                >
-                  {item.name}
-                </Tag>
-              </div>
-            ))}
-          </div>
+                {item.name}
+              </Tag>
+            </div>
+          ))}
         </Col>
       </Row>
     </div>
