7d89ecef709696322561969fce8f2fb76b128b52
Phạm Hồng Kỳ <hongkyhvnh@gmail.com>
2023-05-31 10:02:18 +0000

    Feature/13 ui test details

--- src/pages/TestPage/components/TestDetails/index.tsx
+++ src/pages/TestPage/components/TestDetails/index.tsx
@@ -1,31 +1,37 @@
 import { Col, Row, Typography } from 'antd'
 import React from 'react'
-import Count from '../../../../assets/img/images/count.png'
-import unCount from '../../../../assets/img/images/un-count.png'
+import countImg from '../../../../assets/img/images/count.png'
+import unCountImg from '../../../../assets/img/images/un-count.png'
 import { useNavigate } from 'react-router-dom'
 
 const TestDetails = () => {
   const navigate = useNavigate()
-  const handleMoveExam = () => {
+  const handleGoToExam = () => {
     navigate('/test/exam')
   }
   return (
     <div className="w-full">
-      <div className="bg-[#FFCAD4] lg:py-5 md:py-2 sm:py-2 max-[640px]:py-2 text-center">
-        <Typography className="font-semibold text-[#FB3357] lg:text-5xl md:text-3xl sm:text-3xl max-[640px]:text-3xl">
+      <div className="bg-secondPrimary lg:py-5 sm:py-2 max-sm:py-2 text-center">
+        <Typography className="font-semibold text-primary lg:text-5xl md:text-3xl sm:text-2xl max-sm:text-xl">
           Đề thi N4 - ２０２２年０７月
         </Typography>
       </div>
-      <div className="flex justify-center md:px-20 max-md:px-20 md:my-52 max-md:my-20">
-        <Row className="" gutter={[{ xs: 8, sm: 8, md: 24, lg: 100 }, 20]}>
-          <Col lg={12} md={12} sm={24}>
+      <div className="flex justify-center max-lg:px-20 max-[500px]:px-5 md:my-52 max-md:my-20">
+        <Row
+          className=""
+          gutter={[
+            { xs: 20, sm: 20, md: 50, lg: 100 },
+            { xs: 40, sm: 40 },
+          ]}
+        >
+          <Col md={12} sm={24} xs={24}>
             <div
               className="shadow rounded-[10px] cursor-pointer"
-              onClick={handleMoveExam}
+              onClick={handleGoToExam}
             >
-              <img src={Count} alt="count-img" className="w-full" />
+              <img src={countImg} alt="count-img" className="w-full" />
               <div className="h-44 text-black flex items-center justify-center">
-                <div className="">
+                <div className="text-center max-sm:px-3">
                   <p className="font-semibold text-2xl mb-3">
                     TÍNH GIỜ LÀM BÀI
                   </p>
@@ -34,14 +40,14 @@ const TestDetails = () => {
               </div>
             </div>
           </Col>
-          <Col lg={12} md={12} sm={24}>
+          <Col md={12} sm={24} xs={24}>
             <div
               className="shadow rounded-[10px] cursor-pointer"
-              onClick={handleMoveExam}
+              onClick={handleGoToExam}
             >
-              <img src={unCount} alt="un-count-img" className="w-full" />
+              <img src={unCountImg} alt="un-count-img" className="w-full" />
               <div className="h-44 text-black flex items-center justify-center">
-                <div className="">
+                <div className="text-center max-sm:px-3">
                   <p className="font-semibold text-2xl">
                     KHÔNG TÍNH GIỜ LÀM BÀI
                   </p>
